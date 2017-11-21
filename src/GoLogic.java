aafb8bb4266fbdb9f588fdf5bbebf9deeda76e80
VincentQuillien <vincent.quillien@epitech.eu>
2017-11-21 15:21:28 +0000

    Loop in game logic

--- src/GoLogic.java
+++ src/GoLogic.java
@@ -1,2 +1,9 @@
 public class GoLogic {
+    public static void hasLiberties(int[][] board, int player) {
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[x].length; y++) {
+
+            }
+        }
+    }
 }
