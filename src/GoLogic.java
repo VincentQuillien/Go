31754eb3da208af358ef3df5e536ed62a72d580c
VincentQuillien <vincent.quillien@epitech.eu>
2017-11-28 11:45:05 +0000

    GoLogic

--- src/GoLogic.java
+++ src/GoLogic.java
@@ -1,9 +1,47 @@
-public class GoLogic {
-    public static void hasLiberties(int[][] board, int player) {
-        for (int x = 0; x < board.length; x++) {
-            for (int y = 0; y < board[x].length; y++) {
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
 
-            }
+class Coord {
+    int x;
+    int y;
+
+    Coord(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Coord))
+            return false;
+        Coord other = (Coord) obj;
+        return other.x == this.x && other.y == this.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return x * 7 + y;
+    }
+}
+
+public class GoLogic {
+    private GoPiece.Colour player;
+
+    public Set<Coord> getGroups(int x, int y, GoPiece[][] board) {
+        return hasLiberties(x, y, board, new HashSet<>());
+    }
+
+    private Set<Coord> hasLiberties(int x, int y, GoPiece[][] board, Set<Coord> groups) {
+        GoPiece.Colour piece = board[x][y].getPiece();
+        if (piece == GoPiece.Colour.EMPTY)
+            return null;
+        if (piece != player)
+            return groups;
+        groups.add(new Coord(x, y));
+        for (int i : new int[]{-1, 1}) {
+            hasLiberties(x + i, y, board, groups);
+            hasLiberties(x, y + i, board, groups);
         }
     }
 }
